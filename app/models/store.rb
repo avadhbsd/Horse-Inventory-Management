2d3e3edd47c954dbb71bf81c4e443fa3aa84882c
Sherif Eid <sherif.eid@rubikal.com>
2019-03-25 11:01:55 +0200

    encrypt api keys and secrets in Store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,18 @@
 
 # Represents a Shopify Store.
 class Store < ApplicationRecord
+  crypt_keeper :encrypted_api_key,
+               encryptor: :active_support,
+               key: ENV['CRYPT_KEEPER_KEY'], salt: ENV['CRYPT_KEEPER_SALT']
+
+  crypt_keeper :encrypted_api_pass,
+               encryptor: :active_support,
+               key: ENV['CRYPT_KEEPER_KEY'], salt: ENV['CRYPT_KEEPER_SALT']
+
+  crypt_keeper :encrypted_secret,
+               encryptor: :active_support,
+               key: ENV['CRYPT_KEEPER_KEY'], salt: ENV['CRYPT_KEEPER_SALT']
+
   has_many :orders
   has_many :products
   has_many :product_variants
