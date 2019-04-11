fb677f8955bdf159fb56df6ab7938da1f03bd29f
Sherif Eid <sherif.eid@rubikal.com>
2019-04-11 16:01:24 +0200

    indentation

--- db/migrate/20190411135512_remove_webhook_signature.rb
+++ db/migrate/20190411135512_remove_webhook_signature.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class RemoveWebhookSignature < ActiveRecord::Migration[5.2]
   def change
-		remove_column :stores, :encrypted_webhook_signature
+    remove_column :stores, :encrypted_webhook_signature
   end
 end
